8da763929b2ae4017026c53adebbbf6036f92996
Elissonsilva15 <elissoncrazyng@gmail.com>
2025-08-06 20:42:02 -0300

    Add files via upload

---
+++ componentes/services/api.js
@@ -0,0 +1,7 @@
+
+
+
+const URL = `https://api-1-cdfh.onrender.com/users`
+export default URL
+
+
---
+++ scripts.js
@@ -0,0 +1,108 @@
+import URL from "./componentes/services/api.js"
+const dadoss = document.getElementById('dados')
+const nome = document.getElementById('nome')
+const email = document.getElementById('email')
+const btn = document.getElementById('btn')
+const err = document.getElementById('err')
+
+
+
+btn.addEventListener('click', () => {
+    criarUsuarios()
+})
+
+
+
+
+
+async function criarUsuarios() {
+    const users = {
+        name: nome.value.trim(),
+        email: email.value.trim()
+    }
+
+
+    if (!users.name || !users.email) {
+        err.textContent = "Preencha todos os campos."
+        return
+    }
+
+    try {
+        const resposta = await fetch(URL, {
+            method: 'POST',
+            headers: { "Content-Type": 'application/json' },
+            body: JSON.stringify(users)
+        })
+
+        if (!resposta.ok) throw new Error('Erro ao salvar usuário')
+
+        const dados = await resposta.json()
+
+        nome.value = ''
+        email.value = ''
+        console.log("Usuário salvo:", dados)
+
+  
+        carregarDados()
+
+        err.textContent = "Usuário cadastrado com sucesso!"
+        setTimeout(() => {
+            err.textContent = ""
+        }, 5000)
+
+    } catch (e) {
+        console.error(e)
+        err.textContent = "Erro ao cadastrar usuário."
+    }
+}
+
+
+
+
+async function carregarDados() {
+
+    const usuarios = await fetch(URL, {
+    })
+
+    const dados = await usuarios.json()
+    dadoss.innerHTML = ''
+
+    dados.forEach(usuario => {
+
+        const lista = document.createElement('div')
+        lista.classList.add('lista')
+
+        const nomes = document.createElement('span')
+        nomes.textContent = usuario.name
+
+        const email = document.createElement('span')
+        email.textContent = usuario.email
+        const excluir = document.createElement('button')
+        excluir.classList.add('excluir')
+        excluir.textContent = "Excluir"
+
+        excluir.addEventListener('click', () => {
+            exluirUsuario(usuario.id)
+        })
+
+        dadoss.appendChild(lista)
+        lista.appendChild(nomes)
+        lista.appendChild(email)
+        lista.appendChild(excluir)
+
+
+
+    })
+
+}
+
+
+async function exluirUsuario(id) {
+    const exluirUsuario = await fetch(`${URL}/${id}`, {
+        method: 'DELETE'
+    })
+    carregarDados()
+}
+
+
+carregarDados()
